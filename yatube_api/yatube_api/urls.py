86034820f04c1587562227e158a6e24a635dbf30
Nastasia Dem <oo.cl@ya.ru>
2022-03-25 11:47:17 +0400

    Done

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from django.conf.urls import url
-from rest_framework import permissions
-
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 
 
 urlpatterns = [
@@ -14,25 +9,4 @@ urlpatterns = [
     path('redoc/',
          TemplateView.as_view(template_name='redoc.html'),
          name='redoc'),
-]
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Posts API",
-        default_version='v1',
-        description="Документация для приложения posts проекта Yatube",
-        contact=openapi.Contact(email="admin@yatube.ru"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-urlpatterns += [
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
 ]
